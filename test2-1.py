2150985bb4a4f155bf1c75ddd3a97e4da5b8af7c
maji123 <m18342961655@163.com>
2015-04-15 22:17:30 +0800

    Signed-off-by: maji123 <m18342961655@163.com>

---
+++ test2-1.py
@@ -0,0 +1,15 @@
+#####学生基本信息############ 
+#姓名：马骥 
+#学号：1403050216 
+#班级：通风14-2班  
+###########题目############### 
+物理17.32
+###########代码############### 
+import math 
+m=input('please input a number m=') 
+n=input('please input a number n=') 
+pinput('please input a number p=') 
+d=math.sqrt(n**2-4*m*p) 
+root1 = (-n+ d)/(2*m) 
+root2 = (-n- d)/(2*m) 
+print "root1=,root2=" ,root1,root2 
